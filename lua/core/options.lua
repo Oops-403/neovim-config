886c807330199193be6dcc410a748e7b9cc1e259
shlyn <shp32131@gmail.com>
2022-08-11 15:59:01 +0800

    update core.options

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,34 +1,34 @@
-local global = require("core.global")
+local global = require('core.global')
 local vim = vim
 
 vim.o.termguicolors = true
-vim.o.mouse = "a"
+vim.o.mouse = 'a'
 vim.o.errorbells = true
 vim.o.visualbell = true
 vim.o.hidden = true
-vim.o.fileformats = "unix,mac,dos"
-vim.o.magic = true,
-vim.o.virtualedit = "block"
-vim.o.encoding = "utf-8"
-vim.o.viewoptions = "folds,cursor,curdir,slash,unix"
-vim.o.sessionoptions = "curdir,help,tabpages,winsize"
-vim.o.clipboard = "unnamedplus"
+vim.o.fileformats = 'unix,mac,dos'
+vim.o.magic = true
+vim.o.virtualedit = 'block'
+vim.o.encoding = 'utf-8'
+vim.o.viewoptions = 'folds,cursor,curdir,slash,unix'
+vim.o.sessionoptions = 'curdir,help,tabpages,winsize'
+vim.o.clipboard = 'unnamedplus'
 
-vim.o.wildignorecase = true,
-vim.o.wildignore = ".git,.hg,.svn,*.pyc,*.o,*.out,*.jpg,*.jpeg,*.png,*.gif,*.zip,**/tmp/**,*.DS_Store,**/node_modules/**,**/bower_modules/**"
+vim.o.wildignorecase = true
+vim.o.wildignore = '.git,.hg,.svn,*.pyc,*.o,*.out,*.jpg,*.jpeg,*.png,*.gif,*.zip,**/tmp/**,*.DS_Store,**/node_modules/**,**/bower_modules/**'
 
--- directory = global.cache_dir .. "swap/",
--- backupdir = global.cache_dir .. "backup/",
--- viewdir = global.cache_dir .. "view/",
--- spellfile = global.cache_dir .. "spell/en.uft-8.add",
+-- directory = global.cache_dir .. 'swap/'
+-- backupdir = global.cache_dir .. 'backup/'
+-- viewdir = global.cache_dir .. 'view/'
+-- spellfile = global.cache_dir .. 'spell/en.uft-8.add'
 vim.o.backup = false
 vim.o.writebackup = false
 vim.o.swapfile = false
-vim.o.undodir = global.cache_dir .. "undo/"
-vim.o.backupskip = "/tmp/*,$TMPDIR/*,$TMP/*,$TEMP/*,*/shm/*,/private/var/*,.vault.vim"
+vim.o.undodir = global.cache_dir .. 'undo/'
+vim.o.backupskip = '/tmp/*,$TMPDIR/*,$TMP/*,$TEMP/*,*/shm/*,/private/var/*,.vault.vim'
 
 vim.o.history = 2000
-vim.o.shada = "!,'300,<50,@100,s10,h"
+-- vim.o.shada = '!,'300,<50,@100,s10,h'
 vim.o.smarttab = true
 vim.o.shiftround = true
 
@@ -45,23 +45,23 @@ vim.o.infercase = true
 vim.o.incsearch = true
 vim.o.wrapscan = true
 
-vim.o.complete = ".,w,b,k"
-vim.o.inccommand = "nosplit"
-vim.o.grepformat = "%f:%l:%c:%m"
-vim.o.grepprg = "rg --hidden --vimgrep --smart-case --"
+vim.o.complete = '.,w,b,k'
+vim.o.inccommand = 'nosplit'
+vim.o.grepformat = '%f:%l:%c:%m'
+vim.o.grepprg = 'rg --hidden --vimgrep --smart-case --'
 vim.o.breakat = [[\ \	;:,!?]]
 vim.o.startofline = false
-vim.o.whichwrap = "h,l,<,>,[,],~"
+vim.o.whichwrap = 'h,l,<,>,[,],~'
 vim.o.splitbelow = true
 vim.o.splitright = true
-vim.o.switchbuf = "useopen"
-vim.o.backspace = "indent,eol,start"
-vim.o.diffopt = "filler,iwhite,internal,algorithm:patience"
-vim.o.completeopt = "menuone,noselect"
-vim.o.jumpoptions = "stack"
+vim.o.switchbuf = 'useopen'
+vim.o.backspace = 'indent,eol,start'
+vim.o.diffopt = 'filler,iwhite,internal,algorithm:patience'
+vim.o.completeopt = 'menuone,noselect'
+vim.o.jumpoptions = 'stack'
 
 vim.o.showmode = false
-vim.o.shortmess = "aoOTIcF"
+vim.o.shortmess = 'aoOTIcF'
 
 vim.o.scrolloff = 2
 vim.o.sidescrolloff = 5
@@ -85,9 +85,9 @@ vim.o.cmdwinheight = 5
 vim.o.equalalways = false
 vim.o.laststatus = 2
 
-vim.o.display = "lastline"
-vim.o.showbreak = "↳  "
-vim.o.listchars = "tab:»·,nbsp:+,trail:·,extends:→,precedes:←"
+vim.o.display = 'lastline'
+vim.o.showbreak = '↳  '
+vim.o.listchars = 'tab:»·,nbsp:+,trail:·,extends:→,precedes:←'
 
 vim.o.pumblend = 10
 vim.o.winblend = 10
@@ -96,7 +96,7 @@ vim.o.autowrite = true
 
 vim.o.undofile = true
 vim.o.synmaxcol = 2500
-vim.o.formatoptions = "1jcroql"
+vim.o.formatoptions = '1jcroql'
 
 vim.o.expandtab = true
 vim.o.foldenable = true
@@ -106,7 +106,7 @@ vim.o.autoindent = true
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.softtabstop = 4
-vim.o.breakindentopt = "shift:2,min:20"
+vim.o.breakindentopt = 'shift:2,min:20'
 
 vim.o.wrap = false
 vim.o.linebreak = true
@@ -114,21 +114,21 @@ vim.o.linebreak = true
 vim.o.number = true
 vim.o.relativenumber = true
 
-vim.o.signcolumn = "yes"
+vim.o.signcolumn = 'yes'
 vim.o.conceallevel = 0
-vim.o.concealcursor = "niv"
+vim.o.concealcursor = 'niv'
 
 if global.is_mac then
-	vim.g.python_host_prog = "/usr/bin/python"
-	vim.g.python3_host_prog = "/usr/bin/python3"
+	vim.g.python_host_prog = '/usr/bin/python'
+	vim.g.python3_host_prog = '/usr/bin/python3'
 else 
 	vim.g.clipboard = {
-		name = "macOS-clipboard",
-		copy = { ["+"] = "pbcopy", ["*"] = "pbcopy" },
-		paste = { ["+"] = "pbpaste", ["*"] = "pbpaste" },
+		name = 'macOS-clipboard',
+		copy = { ['+'] = 'pbcopy', ['*'] = 'pbcopy' },
+		paste = { ['+'] = 'pbpaste', ['*'] = 'pbpaste' },
 		cache_enabled = 0,
 	}
-	vim.g.python_host_prog = "/usr/bin/python"
-	vim.g.python3_host_prog = "/usr/local/bin/python3"
+	vim.g.python_host_prog = '/usr/bin/python'
+	vim.g.python3_host_prog = '/usr/local/bin/python3'
 end
 
